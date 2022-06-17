230b11a79a7984848eabf4ea39bd2867801b3b65
Mauro Pignatta <mauroj.pignatta@gmail.com>
2022-06-17 20:13:43 -0300

    Create base project structure.

---
+++ src/main/java/com/tssi/pueblo_pelicula/PuebloPeliculaApplication.java
@@ -0,0 +1,13 @@
+package com.tssi.pueblo_pelicula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuebloPeliculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuebloPeliculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tssi/pueblo_pelicula/PuebloPeliculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tssi.pueblo_pelicula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuebloPeliculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
